ab5aa4336bcf10e08c68d9d9141873ad2687b277
Josh Hannan <josh@inverseparadox.net>
2017-09-11 09:53:47 -0400

    week 3 updates

--- week-3/assignment-2/js/vendor/_init.js
+++ week-3/assignment-2/js/vendor/_init.js
@@ -9,16 +9,21 @@ jQuery(function($) {
 	});
 
 	var header_height = $('header').outerHeight(true);
+	var window_width = $(window).outerWidth(true); 
+	
+	console.log( 'Header Height is: '+header_height );
 
 	$(window).resize(function() {
 
+		window_width = $(window).outerWidth(true); 
 		header_height = $('header').outerHeight(true);
-
+		console.log( window_width );
 	});
 
 	$(window).scroll(function() {
 
 		var scrolling = $(window).scrollTop();
+		console.log( 'Scrolltop is: '+scrolling+'px' );
 
 		if( scrolling >= header_height ) {
 			$('.site-header').addClass('small');
@@ -27,5 +32,49 @@ jQuery(function($) {
 		}
 
 	});
-	
+
+	$('.banner').owlCarousel({
+		items: 4,
+		margin: 15,
+		autoplayTimeout: 7000,
+		autoplay: true,
+		loop: true,
+		responsive : {
+			0 : {
+				items: 1,
+				margin: 10,
+				dots: true,
+				nav: false
+			},
+			480 : {
+				items: 2,
+				margin: 10,
+				dots: true,
+				nav: false
+
+			},
+			769 : {
+				items: 3,
+				margin: 15,
+				dots: true,
+				nav: false
+			},
+			1000 : {
+				items: 4,
+				margin: 15,
+				dots: true,
+				nav: false
+			},
+			1340 : {
+				items: 4,
+				margin: 15,
+				dots: false,
+				nav: true,
+				navText: ['', '']
+			}
+		}
+	});
+
+
+
 });
